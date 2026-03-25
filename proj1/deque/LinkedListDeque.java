6f34b48476c2d235b69d3a1ad1300e87fbaf8e42
momijiee <momijiee@github.com>
2026-03-25 13:08:40 +0800

    Finish LinkedListDeque

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,35 +1,136 @@
 package deque;
 
-public class LinkedListDeque<T> {
-    public void addFirst(T item) {
+import java.util.Iterator;
 
+public class LinkedListDeque<T> {
+    public class ListNode {
+        public T item;
+        public ListNode prev;
+        public ListNode next;
+
+        public ListNode() {
+
+        }
+
+        public ListNode(T item) {
+            this.item = item;
+        }
+    }
+
+    private int size;
+    private final ListNode sentinel;
+
+    public LinkedListDeque(){
+        sentinel = new ListNode();
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+    public LinkedListDeque(T item){
+        sentinel = new ListNode();
+        ListNode node = new ListNode(item);
+        sentinel.next = node;
+        sentinel.prev = node;
+        node.next = sentinel;
+        node.prev = sentinel;
+        size = 1;
+    }
+
+    public void addFirst(T item) {
+        ListNode node = new ListNode(item);
+        node.next = sentinel.next;
+        node.next.prev = node;
+        node.prev = sentinel;
+        sentinel.next = node;
+        size += 1;
     }
 
     public void addLast(T item) {
-
+        ListNode node = new ListNode(item);
+        node.prev = sentinel.prev;
+        node.prev.next = node;
+        node.next = sentinel;
+        sentinel.prev = node;
+        size += 1;
     }
 
     public boolean isEmpty() {
-
+        return size == 0;
     }
 
     public int size() {
-
+        return size;
     }
 
     public void printDeque() {
-
+        ListNode p = sentinel.next;
+        if (p == sentinel) {
+            System.out.println("empty deque");
+        }
+        while (p.next != sentinel) {
+            System.out.print(p.item + " -> ");
+            p = p.next;
+        }
+        System.out.println(p.item);
     }
 
     public T removeFirst() {
-
+        if (size == 0) {
+            return null;
+        }
+        ListNode removedNode = sentinel.next;
+        sentinel.next = removedNode.next;
+        removedNode.next.prev = sentinel;
+        size -= 1;
+        return removedNode.item;
     }
 
     public T removeLast() {
-
+        if (size == 0) {
+            return null;
+        }
+        ListNode removedNode = sentinel.prev;
+        sentinel.prev =removedNode.prev;
+        removedNode.prev.next = sentinel;
+        size -= 1;
+        return removedNode.item;
     }
 
     public T get(int index) {
-        
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        ListNode p = sentinel;
+        for (int i = 0; i < index + 1; i++) {
+            p = p.next;
+        }
+        return p.item;
     }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return getRecursiveHelper(sentinel.next, index);
+    }
+
+    /** Helper function for getRecursive function. */
+    private  T getRecursiveHelper(ListNode p, int index) {
+        if (index == 0) {
+            return p.item;
+        }
+        else {
+            return getRecursiveHelper(p.next, index - 1);
+        }
+    }
+
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    public boolean equals(Object o) {
+        return true;
+    }
+
 }
