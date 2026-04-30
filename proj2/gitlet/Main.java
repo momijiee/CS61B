bcc4e3d7c25c58eb39e07ea0d3a18e0602773fc2
momijiee <momijiee@github.com>
2026-04-30 17:11:41 +0800

    brief start of proj2

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,7 +1,9 @@
 package gitlet;
 
+import org.junit.Assert;
+
 /** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+ *  @author momiji
  */
 public class Main {
 
@@ -9,16 +11,47 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
+        // Handle empty args
+        if (args.length == 0) {
+            printThenExit("Please enter a command.");
+        }
+
+        // Handle the first arg
         String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
+        switch (firstArg) {
+            case "init": {
                 // TODO: handle the `init` command
+
                 break;
+            }
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
-            // TODO: FILL THE REST IN
+            case "commit":
+                // handle `commit [message]` command
+                validateNumArgs(args, 2);
+                String message = args[1];
+
+                break;
+            case "checkout":
+
+                break;
+            case "log":
+
+                break;
+            default:
+                printThenExit("No command with that name exists.");
         }
+    }
+
+    private static void validateNumArgs(String[] args, int n) {
+        if (args.length != n) {
+            printThenExit("Incorrect operands.");
+        }
+    }
+
+    private static void printThenExit(String message) {
+        System.out.println(message);
+        System.exit(0);
     }
 }
